c79ef48b93ae86263d697364b8ec98465f25d7d8
aleksandrasucho <aleksandrasucho@gmail.com>
2023-10-22 14:02:17 +0100

    Add fighting options to the story

--- assets/script/story.js
+++ assets/script/story.js
@@ -16,11 +16,44 @@ const hallwayButton = document.getElementById('hallway-button');
 
 // Add event listeners to buttons
 
-// When runButton is clicked, hide diningRoomModal and show escapeModal
+// When door2Button is clicked, hide hauntedMansionModal and show basementModal
 door2Button.addEventListener('click', function() {
-    // Hide the haunted mansion modal
     hauntedMansionModal.style.display = 'none';
-    // Show the basement modal
     basementModal.style.display = 'block';
 });
 
+// When door1Button is clicked, hide hauntedMansionModal and show diningRoomModal
+door1Button.addEventListener("click", function() {
+    hauntedMansionModal.style.display = 'none';
+    diningRoomModal.style.display = 'block';
+});
+
+// When runButton is clicked, hide diningRoomModal and show escapeModal
+runButton.addEventListener('click', function() {
+    diningRoomModal.style.display = 'none';
+    escapeModal.style.display = 'block';
+});
+
+// When defendButton is clicked, hide diningRoomModal and show defendModal
+defendButton.addEventListener('click', function() {
+    diningRoomModal.style.display = 'none';
+    defendModal.style.display = 'block';
+})
+
+// When basementButton is clicked, hide defendModal and show basementModal
+basementButton.addEventListener('click', function() {
+    defendModal.style.display = 'none';
+    basementModal.style.display = 'block';
+});
+
+// When reasonButton is clicked, hide diningRoomModal and show reasonModal
+reasonButton.addEventListener('click', function() {
+    diningRoomModal.style.display = 'none';
+    reasonModal.style.display = 'block';
+})
+
+// When reasonBasementButton is clicked, hide reasonModal and show basementModal
+reasonBasementButton.addEventListener('click', function() {
+    reasonModal.style.display = 'none';
+    basementModal.style.display = 'block';
+});
